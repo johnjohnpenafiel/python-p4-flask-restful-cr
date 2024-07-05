6e34d0d34ee70f333331621b7637743e3b6e23c4
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-07-05 16:39:11 -0400

    Restful CR

--- server/app.py
+++ server/app.py
@@ -17,7 +17,48 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    
+    def get(self):
+
+        response = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        return response, 200
+
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+   
+    def get(self):
+        news_letters = Newsletter.query.all()
+        return [n.to_dict() for n in news_letters], 200
+
+
+    def post(self):
+        new_record = Newsletter(
+            title=request.form.get('title'),
+            body=request.form.get('body')
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+        
+        return new_record.to_dict(), 200
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterById(Resource):
+    
+    def get(self, id):
+
+        newsletter = Newsletter.query.fitler(Newsletter.id == id).first()
+
+        return newsletter.to_dict(), 200
+
+api.add_resource(NewsletterById, '/newsletters/<int:id>')
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
